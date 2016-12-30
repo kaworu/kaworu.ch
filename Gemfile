a5a0ca5fb2043565e733b30d3c5cf6b533bc21d8
Alexandre Perrin <alex@kaworu.ch>
2016-12-30 17:17:23 +0100

    Switch from pygment.rb to rouge, Work in progress.
    Cleaner overall but heavy lifting still needed from the css
    (_syntax.scss) part.

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'builder'     # for feeds
-gem 'pygments.rb' # for syntax highlighting
-gem 'compass'     # for advanced CSS
 gem 'nanoc', '~> 4.0'
-gem 'nokogiri'    # for parsing HTML
 gem 'rake'
 gem 'sass'
-gem 'systemu'     # for invoking rsync etc
+gem 'compass'  # for advanced CSS
+gem 'rouge'    # for syntax highlighting
+gem 'builder'  # for feeds
+gem 'nokogiri' # for parsing HTML
+gem 'systemu'  # for invoking rsync etc
--- lib/filters/octohilight.rb
+++ lib/filters/octohilight.rb
@@ -1,4 +1,4 @@
-# Pygments Nanoc filter producing Twitter Bootstrap panels for code
+# Rouge Nanoc filter producing Twitter Bootstrap panels for code
 # highlighting.
 #
 # Using:
@@ -9,38 +9,36 @@
 #   where CODE is some code and options is an optional JSON string.
 #
 # Options:
-#   - lang: (required) the syntaxe to highligh
-#               (ex. ruby, javascript etc.). See the Pygment documentation
-#               (http://pygments.org/docs/lexers/) for the full list.
-#   - title:    (optional) the panel's title.
-#   - linenos:  (true by default) if false the line numbers will not be
-#               generated in the table
-#   - href:     (optional) an adresse where the code snippet can be downloaded,
-#               a download button will be generated.
+#   - lang: (optional)
+#     the syntaxe to highligh (eg. ruby). See the Rouge supported languages
+#     list at https://github.com/jneen/rouge/wiki/List-of-supported-languages-and-lexers
+#   - title: (optional)
+#     the panel's title.
+#   - linenos: (optional, default: true)
+#     if false the line numbers will not be generated in the table
+#   - href: (optional)
+#     an adresse where the code snippet can be downloaded, a download button
+#     will be generated.
 #
 # Example:
 #   ```{"lang": "ruby", "title": "The Classic"}
 #   puts "Hello World!"
 #   ```
-#
-# Hacking:
-#   If you want to hack this file and move it around be sure to check
-#   PYGMENTS_CACHE_DIR which is hardcoded below !
 
 class OctoHighlight < Nanoc::Filter
   require 'json'
+  require 'rouge'
 
   identifier :octohl
 
   # implement the filter.
   def run(content, params = {})
-    content.gsub /```([^\n]*)\n(.+?)```$/m  do
+    content.gsub /```([^\n]*)\n(.+?)```$/m do
       optstring = $1
       code      = $2
       options   = optstring.empty? ? {} : JSON.parse(optstring)
       txt = strip(code)
       txt = highlight(txt, options) if options['lang']
-      txt = tableize(txt, options)
       panelize(txt, options)
     end
   end
@@ -52,7 +50,13 @@ class OctoHighlight < Nanoc::Filter
 
   # highlight the given text using the syntaxe provided in options.
   def highlight(content, options)
-    HighlightCode::highlight(content, options['lang'])
+    lang = options['lang']
+    formatter = Rouge::Formatters::HTML.new
+    unless options['linenos'] === false
+      formatter = Rouge::Formatters::HTMLTable.new(formatter)
+    end
+    lexer = Rouge::Lexer.find_fancy(lang, content) || Rouge::Lexers::PlainText
+    formatter.format(lexer.lex(content))
   end
 
   # generate a bootstrap panel, handling link (href) option and title.
@@ -77,78 +81,9 @@ class OctoHighlight < Nanoc::Filter
     (<<-EOL
       <figure class="code panel panel-default">
         %s
-        <div class="panel-body">%s</div>
+        <div class="panel-body highlight">%s</div>
       </figure>
      EOL
     ) % [caption, content]
-  end
-
-  # wrap the highlighted code into a table, handling the linenos option.
-  def tableize(content, options)
-    lines = ''
-    code  = ''
-    content.lines.each_with_index do |line, index|
-      lines += "<span class=\"line-number\">%d</span>\n" % (index + 1)
-      code  += "<span class=\"line\">#{line}</span>"
-    end
-    lines = '<td class="gutter"><pre class="line-numbers">%s</pre></td>' % lines
-    code  = '<td class="code"><pre><code class="%s">%s</code></pre></td>' % [options['lang'], code]
-    table = '<table><tr>%s</tr></table>' % (options['linenos'] === false ? code : lines + code)
-    '<div class="highlight">%s</div>' % table
-  end
-
-  # HighlightCode has been copied and hacked from (http://octopress.org/)
-  #
-  # (The MIT License)
-  #
-  # Copyright © 2009-2013 Brandon Mathis
-  #
-  # Permission is hereby granted, free of charge, to any person obtaining a
-  # copy of this software and associated documentation files (the ‘Software’),
-  # to deal in the Software without restriction, including without limitation
-  # the rights to use, copy, modify, merge, publish, distribute, sublicense,
-  # and/or sell copies of the Software, and to permit persons to whom the
-  # Software is furnished to do so, subject to the following conditions:
-  #
-  # The above copyright notice and this permission notice shall be included in
-  # all copies or substantial portions of the Software.
-  #
-  # THE SOFTWARE IS PROVIDED ‘AS IS’, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-  # DEALINGS IN THE SOFTWARE.
-  require 'digest/md5'
-  require 'fileutils'
-  require 'pygments'
-
-  PYGMENTS_CACHE_DIR = File.expand_path('../../../tmp/pygments-cache', __FILE__)
-  FileUtils.mkdir_p(PYGMENTS_CACHE_DIR)
-
-  module HighlightCode
-    def self.highlight(str, lang)
-      pygments(str, lang).match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '') #strip out divs <div class="highlight">
-    end
-
-    def self.pygments(code, lang)
-      if defined?(PYGMENTS_CACHE_DIR)
-        path = File.join(PYGMENTS_CACHE_DIR, "#{lang}-#{Digest::MD5.hexdigest(code)}.html")
-        if File.exist?(path)
-          highlighted_code = File.read(path)
-        else
-          begin
-            highlighted_code = Pygments.highlight(code, lexer: lang, formatter: 'html', options: {encoding: 'utf-8', startinline: true})
-          rescue MentosError
-            raise "Pygments can't parse unknown language: #{lang}."
-          end
-          File.open(path, 'w') {|f| f.print(highlighted_code) }
-        end
-      else
-        highlighted_code = Pygments.highlight(code, lexer: lang, formatter: 'html', options: {encoding: 'utf-8', startinline: true})
-      end
-      highlighted_code
-    end
   end
 end
